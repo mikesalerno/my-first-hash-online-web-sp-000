750ca417d95eb30a7d1a6b15696f91ea9586daa2
Michael <wtfdic@hotmail.com>
2020-12-01 02:30:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,8 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "muskets" => 2
                  }
   # add 2 muskets to the shipping_manifest hash below
 
